f64add4644f746bf40dbf88595cd23c2a4ce3154
Justin Truong <justin.truong@seattlecolleges.edu>
2022-01-26 03:50:56 -0800

    changes

---
+++ js/data-processing.js
@@ -0,0 +1,112 @@
+
+//put reference to where the function came from. 
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+  }
+
+
+
+
+
+
+
+
+//use location object to access querystring (address bar)
+const queryString = window.location.search;
+let myData = '';//will store data for output 
+let myCart = '';//will store info abouit Cart contents   
+let myTotal = 0;//cost of items in cart
+
+
+if(queryString != "") { //process data
+    
+        //separate querystring parameters
+        const urlParams = new URLSearchParams(queryString);
+
+        urlParams.forEach(function(value, key) {
+            
+            
+            if(key=="Cart") {//Adds cart items 
+                //alert(value);
+
+            switch(value){
+
+                case "Widget":
+                    myCart += "Widget: $3.99<br />";
+                    myTotal += 3.99;
+                break;
+
+                case "Sprocket":
+                    myCart += "Sprocket: $5.99<br />";
+                    myTotal += 5.99;
+                break;
+
+
+                case "Thingy":
+                    myCart += "Thingy: $1.99<br />";
+                    myTotal += 1.99;
+                break;
+            }
+
+
+            } else {
+                
+                /*we will apply title case to? 
+                keys, First_Name, Last_Name, Address, City
+
+                value = titleCase(value);
+
+                */
+
+                switch(key) {
+                    case "First_Name":
+                    case "Last_Name":
+                    case "Address":
+                    case "City":
+                        value = titleCase(value); 
+                    break;
+                }
+                
+                
+                //build the shipping info 
+                //will replace underscore with space
+                //stackflow.com/questions/542232/in-javascr
+                key = key.split("_").join(" ");
+                myData += `<p>${key}: ${value}</p>`; //you can put data in a string that you can manage. 
+                
+                
+                //console.log(value, key);
+            }
+            
+            });
+        myCart = `<h3>Cart Contents</h3>
+                  <p>${myCart}</p>
+                  <p>Total: $${myTotal}</p>
+                  `;
+        
+        myData = myCart + + '<h3>Shipping Info</h3>' + myData; 
+                  myData += `<p><a href="index.html">CLEAR</a</p>`;
+        
+        document.getElementById("output").innerHTML = myData; 
+
+
+}    
+//output to console    
+//console.log(queryString);
+    
+//separate querystring parameters
+//const urlParams = new URLSearchParams(queryString);
+
+/*
+    Print all data to id of output on page
+
+
+
+*/
+
+
+ 
--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 //alert("test");
+
+/*  //use location object to access querystring (address bar)
+const queryString = window.location.search;
+    
+//output to console    
+console.log(queryString);
+    
+//separate querystring parameters
+const urlParams = new URLSearchParams(queryString);
+
+urlParams.forEach(function(value, key) {
+    console.log(value, key);
+  });
+ */
+
+  document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+
+  document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
+
+  //manages mobile nav
+
+  function myFunction() { 
+    var x = document.getElementById("MyTopnav");
+    if (x.className === "topnav") {
+      x.classname += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+    
+  }
